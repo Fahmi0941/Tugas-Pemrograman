9dc6a8b2ffd4fc3685adc97b9431056a64700df6
Fahmi0941 <mie.suedapp@gmail.com>
2018-09-25 13:54:02 +0700

    Tugas pemrograman Calculator

---
+++ Calct/Calct/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Calct
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtHasil = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnHit = new System.Windows.Forms.Button();
+            this.cbOperasi = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Reset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(88, 24);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 0;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(88, 65);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 20);
+            this.txtB.TabIndex = 1;
+            // 
+            // txtHasil
+            // 
+            this.txtHasil.Location = new System.Drawing.Point(88, 203);
+            this.txtHasil.Name = "txtHasil";
+            this.txtHasil.Size = new System.Drawing.Size(100, 20);
+            this.txtHasil.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nilai A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nilai B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Operasi";
+            // 
+            // btnHit
+            // 
+            this.btnHit.Location = new System.Drawing.Point(140, 155);
+            this.btnHit.Name = "btnHit";
+            this.btnHit.Size = new System.Drawing.Size(48, 23);
+            this.btnHit.TabIndex = 6;
+            this.btnHit.Text = "Hitung";
+            this.btnHit.UseVisualStyleBackColor = true;
+            this.btnHit.Click += new System.EventHandler(this.btnHit_Click);
+            // 
+            // cbOperasi
+            // 
+            this.cbOperasi.FormattingEnabled = true;
+            this.cbOperasi.Items.AddRange(new object[] {
+            "Tambah",
+            "Kurang",
+            "Kali",
+            "Bagi"});
+            this.cbOperasi.Location = new System.Drawing.Point(88, 105);
+            this.cbOperasi.Name = "cbOperasi";
+            this.cbOperasi.Size = new System.Drawing.Size(100, 21);
+            this.cbOperasi.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Hasil";
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(88, 155);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(46, 23);
+            this.Reset.TabIndex = 9;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 281);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cbOperasi);
+            this.Controls.Add(this.btnHit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtHasil);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtHasil;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnHit;
+        private System.Windows.Forms.ComboBox cbOperasi;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Reset;
+    }
+}
+
---
+++ Calct/Calct/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calct
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHit_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(txtA.Text);
+            int b = int.Parse(txtB.Text);
+            int hitung;
+            string hasil;
+
+            if (cbOperasi.SelectedIndex == 0)
+            {
+                hitung = a + b;
+                hasil = hitung.ToString();
+                txtHasil.Text = hasil;
+            }
+
+            if (cbOperasi.SelectedIndex == 1)
+            {
+                hitung = a - b;
+                hasil = hitung.ToString();
+                txtHasil.Text = hasil;
+            }
+
+            if (cbOperasi.SelectedIndex == 2)
+            {
+                hitung = a * b;
+                hasil = hitung.ToString();
+                txtHasil.Text = hasil;
+            }
+
+            if (cbOperasi.SelectedIndex == 3)
+            {
+                hitung = a / b;
+                hasil = hitung.ToString();
+                txtHasil.Text = hasil;
+            }
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            txtA.Text = " ";
+            txtB.Text = " ";
+            txtHasil.Text = " ";
+        }
+    }
+}
